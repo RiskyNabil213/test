04eca117771093108cab59c3a653802d047fab93
RiskyNabil213 <risky_pahlevi_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 14:57:26 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,11 @@
+public class Tes {
+
+    public static void main(String[] args) {
+        System.out.println("hai guys ini tes ya");
+        System.out.println("nama saya Risky Nabil Pahlevi");
+        System.out.println("alamat saya di mojokerto jln riyanto");
+        System.out.println("asal smp saya di mts al amin mojokerto");
+        System.out.println("no hp saya:085790519397");
+        
+    }
+}
